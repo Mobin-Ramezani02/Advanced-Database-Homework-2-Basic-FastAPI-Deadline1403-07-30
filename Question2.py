a352bb42808c34402ebbb87a4bb79272c8cce4c3
Mobin Ramezani <120684576+Mobin-Ramezani02@users.noreply.github.com>
2025-01-02 00:13:34 +0330

    Question2.py

--- Question2.py
+++ Question2.py
@@ -16,49 +16,35 @@ class NumberBody(BaseModel):
 
 
 def q2(number: int):
-    change_sign = True
-    sign = Decimal(1)
-    fact_start = 3
-    first_num = 2
-    second_num = 9
     result = Decimal(0)
-
-    for _ in range(number):
-        fact = Decimal(math.factorial(fact_start))
-        denominator = Decimal(first_num + second_num)
+    sign = -1
+    for i in range(number):
+        sign *= -1
+        numerator = (2 * i) + 3
+        denominator = Decimal(i + 2) + Decimal(9 - (2 * i))
 
         if denominator == 0:
             continue
-        else:
-            result += sign * (fact / denominator)
 
-        fact_start += 2
-        first_num += 1
-        second_num -= 2
-
-        if change_sign:
-            sign = Decimal(-1)
-            change_sign = False
-        else:
-            sign = Decimal(1)
-            change_sign = True
+        term_value = Decimal(math.factorial(numerator) / denominator)
+        result += Decimal(sign) * term_value
 
     return result
 
 
 @app.get("/q2/{n}")
-def question2_path(n: int):
+async def question2_path(n: int):
     result = q2(n)
     return {"result with (path parameter)": result}
 
 
 @app.get("/q2/")
-def question2_query(n: int):
+async def question2_query(n: int):
     result = q2(n)
     return {"result with (query parameter)": result}
 
 
 @app.post("/q2/")
-def question2_body(n: NumberBody):
+async def question2_body(n: NumberBody):
     result = q2(n.n)
     return {"result with (body)": result}
